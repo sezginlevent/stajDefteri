f6c848db633a33f3fb7c66144326b6cdc90203a2
Sezgin Levent <slevent1907@gmail.com>
2022-05-27 22:03:21 +0300

    HOTFIX 0.4

--- stajDefteri/Form1.cs
+++ stajDefteri/Form1.cs
@@ -8,13 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Threading;
 
 namespace stajDefteri
 {
     public partial class Form1 : Form
     {
         string allText;
-        string date;
         public Form1()
         {
             InitializeComponent();
@@ -23,11 +23,9 @@ namespace stajDefteri
 
         private void button3_Click(object sender, EventArgs e)
         {
-            string path = @"C:\\stajdefterim\\stajDefterim" + dateDate.Value.Date.ToShortDateString()+ ".txt";
-            File.Create(path);
             string path2 = @"C:\\stajdefterim\\stajDefterim" + dateDate.Value.Date.ToShortDateString() + ".txt";
             allText = "Tarih: " + dateDate.Value.ToString() + " Başlık: " + tbTitle.Text + " Özet: " + tbSummary.Text + " Yapılan İş: " + tbJobText.Text;
-            File.WriteAllText(path2, dateDate.Value.ToString());
+            File.WriteAllText(path2, allText);
             MessageBox.Show("Günlük iş eklenildi.");
         }
     }
